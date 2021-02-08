8c22d3cf45bd9c4f90af65e0f0061b4a77d4fcc6
Richard W. L. Brehmer <richard@bnu.com.br>
2021-02-08 17:37:02 -0300

    Changed server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,35 @@
-import express, {Request, Response} from 'express';
-import cors from 'cors';
+// import express, {Request, Response} from 'express';
+// import cors from 'cors';
 
 import routes from './routes';
 import './database/connection';
 
-const app = express();
-app.use(cors());
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use(routes);
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.json({
+//     'status': 'App running'
+//   });
+// });
+
+// app.listen(3333, () => {
+//   console.log('App running...')
+// });
+
+import express from 'express';
+const app = express()
+
 app.use(express.json());
 app.use(routes);
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({
-    'status': 'App running'
-  });
+
+const PORT : string|number = process.env.PORT || 5000;
+
+app.use("*",(req, res) =>{
+    res.send("<h1>Welcome to your simple server! Awesome right</h1>");
 });
 
-app.listen(3333, () => {
-  console.log('App running...')
-});
+app.listen(PORT,() => console.log(`hosting @${PORT}`));
